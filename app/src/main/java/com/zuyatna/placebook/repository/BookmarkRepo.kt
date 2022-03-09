fd19256aa41ea20d0736d3adb5ca885649202c2a
Suyatna <zuyatna@gmail.com>
2022-03-09 15:48:34 +0700

    chapter16 - creating the repository

---
+++ app/src/main/java/com/zuyatna/placebook/repository/BookmarkRepo.kt
@@ -0,0 +1,28 @@
+package com.zuyatna.placebook.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.zuyatna.placebook.db.BookmarkDao
+import com.zuyatna.placebook.db.PlaceBookDatabase
+import com.zuyatna.placebook.model.Bookmark
+
+class BookmarkRepo(context: Context) {
+
+    private val db = PlaceBookDatabase.getInstance(context)
+    private val bookmarkDao: BookmarkDao = db.bookmarkDao()
+
+    fun addBookmark(bookmark: Bookmark): Long? {
+        val newId = bookmarkDao.insertBookmark(bookmark)
+        bookmark.id = newId
+        return newId
+    }
+
+    fun createBookmark(): Bookmark {
+        return Bookmark()
+    }
+
+    val allBookmarks: LiveData<List<Bookmark>>
+        get() {
+            return bookmarkDao.loadAll()
+        }
+}
