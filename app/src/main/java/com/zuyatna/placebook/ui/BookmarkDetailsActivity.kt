f90e88aa2efe52e60b69554c6f07ceaadf82ad94
Suyatna <zuyatna@gmail.com>
2022-05-25 13:59:22 +0700

    chapter17 - retrieving the bookmark view

--- app/src/main/java/com/zuyatna/placebook/ui/BookmarkDetailsActivity.kt
+++ app/src/main/java/com/zuyatna/placebook/ui/BookmarkDetailsActivity.kt
@@ -2,12 +2,16 @@ package com.zuyatna.placebook.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.zuyatna.placebook.R
 import com.zuyatna.placebook.databinding.ActivityBookmarkDetailsBinding
+import com.zuyatna.placebook.viewmodel.BookmarkDetailsViewModel
 
 class BookmarkDetailsActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityBookmarkDetailsBinding
+    private val bookmarkDetailsViewModel by viewModels<BookmarkDetailsViewModel>()
+    private var bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,14 @@ class BookmarkDetailsActivity : AppCompatActivity() {
 
     private fun setupToolbar() {
         setSupportActionBar(dataBinding.toolbar)
+    }
+
+    private fun populateImageView() {
+        bookmarkDetailsView?.let { bookmarkView ->
+            val placeImage = bookmarkView.getImage(this)
+            placeImage?.let {
+                dataBinding.imageViewPlace.setImageBitmap(placeImage)
+            }
+        }
     }
 }
