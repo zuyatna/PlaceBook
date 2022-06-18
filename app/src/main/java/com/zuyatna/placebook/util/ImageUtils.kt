b2fd59802833293220d3b3708d428527485e9b96
Suyatna <zuyatna@gmail.com>
2022-06-18 20:27:39 +0700

    chapter18 - changes an image, process the capture results, calculate in sample size photo

--- app/src/main/java/com/zuyatna/placebook/util/ImageUtils.kt
+++ app/src/main/java/com/zuyatna/placebook/util/ImageUtils.kt
@@ -48,4 +48,23 @@ object ImageUtils {
             e.printStackTrace()
         }
     }
+
+    private fun calculateInSampleSize(
+        width: Int,
+        height: Int,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Int {
+        var inSampleSize = 1
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+            while (halfHeight / inSampleSize >= reqHeight &&
+                    halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
+    }
 }
