b459626064a29b771db75758aecaa42a204957a7
Suyatna <zuyatna@gmail.com>
2022-06-24 15:30:46 +0700

    chapter18 - capturing an image, process the capture results, setImage in BookmarkDetailsViewModel

--- app/src/main/java/com/zuyatna/placebook/viewmodel/BookmarkDetailsViewModel.kt
+++ app/src/main/java/com/zuyatna/placebook/viewmodel/BookmarkDetailsViewModel.kt
@@ -2,6 +2,7 @@ package com.zuyatna.placebook.viewmodel
 
 import android.app.Application
 import android.content.Context
+import android.graphics.Bitmap
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
@@ -71,6 +72,12 @@ class BookmarkDetailsViewModel(application: Application) : AndroidViewModel(appl
     ) {
         fun getImage(context: Context) = id?.let {
             ImageUtils.loadBitmapFromFile(context, Bookmark.generateImageFilename(it))
+        }
+
+        fun setImage(context: Context, image: Bitmap) {
+            id?.let {
+                ImageUtils.saveBitmapToFile(context, image, Bookmark.generateImageFilename(it))
+            }
         }
     }
 }
